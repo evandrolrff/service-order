6c3de6797c082cef62c44f5eca801e01eb771add
evandrolrff <evandrolrff@gmail.com>
2025-08-09 08:30:21 -0300

    first commit

---
+++ OD.API/OD.API/Database/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OD.API.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Define DbSets for your entities here
+        // public DbSet<YourEntity> YourEntities { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Configure your entity mappings here
+            // modelBuilder.Entity<YourEntity>().ToTable("YourTableName");
+        }
+    }
+}
